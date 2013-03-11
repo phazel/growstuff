2f50434fab4a1b476ca13a771417052023bc23a7
gnattery <gnattitude@gmail.com>
2013-03-11 13:05:25 +1100

    Added validation for amount

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,6 +1,9 @@
 class Payment < ActiveRecord::Base
-  attr_accessible :amount, :paid_period_begins, :paid_period_ends, :payer_id, :recipient_id
+  attr_accessible :amount, :payer_id, :recipient_id
 
   belongs_to :payer, :class_name => 'Member'
   belongs_to :recipient, :class_name => 'Member'
+
+  validates :amount, :presence => true,
+    :numericality => {:greater_than_or_equal_to => 10}
 end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -6,4 +6,23 @@ describe Payment do
     @payment = FactoryGirl.create(:payment)
     @payment.payer.should be_an_instance_of Member
   end
+
+  context "amount" do
+    it "must not be blank" do
+      @payment = FactoryGirl.build(:payment, :amount => nil)
+      @payment.save.should_not be_true
+    end
+
+    it "must be numeric" do
+      @payment = FactoryGirl.build(:payment, :amount => "aaa")
+      @payment.save.should_not be_true
+    end
+
+    it "must be greater than or equal to 10" do
+      @payment = FactoryGirl.build(:payment, :amount => 3)
+      @payment.save.should_not be_true
+      @payment2 = FactoryGirl.build(:payment, :amount => 10)
+      @payment2.save.should be_true
+    end
+  end
 end
